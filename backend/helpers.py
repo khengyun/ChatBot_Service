d91804fe963924e0807d2d2d98b1bcd0367b50fc
khoi03 <khoivtvn002@gmail.com>
2024-07-29 14:15:23 +0700

    update and remove not important code

--- backend/helpers.py
+++ backend/helpers.py
@@ -55,17 +55,3 @@ def query_rag(query_text: str) -> str:
 
     print(results)
     return context_text, results
-
-
-
-def main():
-    # Create CLI.
-    parser = argparse.ArgumentParser()
-    parser.add_argument("query_text", type=str, help="The query text.")
-    args = parser.parse_args()
-    query_text = args.query_text
-
-    context = query_rag(query_text)
-
-if __name__ == "__main__":
-    main()
